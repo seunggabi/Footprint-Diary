c84d7e464a963444f078bda4aa044cf7c998b58a
seunggabi <seunggabi@naver.com>
2016-07-09 22:14:02 +0900

    test

---
+++ Diary/Helper/Helper.h
@@ -0,0 +1,19 @@
+//
+//  Functions.h
+//  Diary
+//
+//  Created by 김승갑 on 2016. 7. 9..
+//  Copyright © 2016년 mju12345. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <DaumMap/MTMapReverseGeoCoder.h>
+
+@interface Helper : NSObject
+
+@property (strong, nonatomic) NSString *apiKey;
+
++(Helper *) getInstance;
+-(NSString *) getToday;
+
+@end
