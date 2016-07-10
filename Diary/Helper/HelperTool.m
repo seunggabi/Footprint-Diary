7cb5f32ee540b16bcdb416bcb3788e39d8be96d1
seunggabi <seunggabi@naver.com>
2016-07-11 00:26:27 +0900

    New HelperTool

---
+++ Diary/Helper/HelperTool.m
@@ -0,0 +1,13 @@
+//
+//  HelperTool.m
+//  Diary
+//
+//  Created by 김승갑 on 2016. 7. 11..
+//  Copyright © 2016년 mju12345. All rights reserved.
+//
+
+#import "HelperTool.h"
+
+@implementation HelperTool
+
+@end
