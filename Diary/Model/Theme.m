5fb84ead22fa13d8f05163e3df0dc93dd8f11629
C GUARD P <pcguard@p0003-mba13.local>
2016-07-12 13:48:18 +0900

    날씨, 테마 삽입

--- Diary/Model/Theme.m
+++ Diary/Model/Theme.m
@@ -27,7 +27,26 @@
     return t;
 }
 -(NSDictionary *) getObj {
-    return @{@"th_id":th_id, @"th_name":th_name, @"th_top":th_top, @"th_bottom":th_bottom, @"th_font":th_font, @"th_main":th_main};
+    return @{@"tree.jpg":th_id, @"Tree":th_name, @"#aaaaaa":th_top, @"#aaaaaa":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"balloon.jpg":th_id, @"Balloon":th_name, @"#ecdfbd":th_top, @"#ecdfbd":th_bottom, @"#444444":th_font, @"#000000":th_main};
+    return @{@"bottle.jpg":th_id, @"Bottle":th_name, @"#98a590":th_top, @"#98a590":th_bottom, @"#ffffff":th_font, @"#000000":th_main};
+    return @{@"cactus.jpg":th_id, @"Cactus":th_name, @"#85c4b9":th_top, @"#85c4b9":th_bottom, @"#ffffff":th_font, @"#000000":th_main};
+    return @{@"city.jpg":th_id, @"City":th_name, @"#aaaaaa":th_top, @"#aaaaaa":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"cloud.jpg":th_id, @"Cloud":th_name, @"#aaaaaa":th_top, @"#aaaaaa":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"FerrisWheel.jpg":th_id, @"FerrisWheel":th_name, @"#69d2e7":th_top, @"#69d2e7":th_bottom, @"#ffffff":th_font, @"#000000":th_main};
+    return @{@"firework.jpg":th_id, @"Firework":th_name, @"#aaaaaa":th_top, @"#aaaaaa":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"flower.jpg":th_id, @"Flower":th_name, @"#e8928d":th_top, @"#e8928d":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"greek.jpg":th_id, @"Greek":th_name, @"#ffe6e6":th_top, @"#ffe6e6":th_bottom, @"#ff8080":th_font, @"#000000":th_main};
+    return @{@"icecream.jpg":th_id, @"Icecream":th_name, @"#ffffff":th_top, @"#ffffff":th_bottom, @"#000000":th_font, @"#ffffff":th_main};
+    
+    return @{@"ocean.jpg":th_id, @"Ocean":th_name, @"#ffffff":th_top, @"#ffffff":th_bottom, @"#000000":th_font, @"#ffffff":th_main};
+    return @{@"rainywindow.jpg":th_id, @"Rainywindow":th_name, @"#000000":th_top, @"#000000":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"sea.jpg":th_id, @"Sea":th_name, @"#28abe3":th_top, @"#28abe3":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"sea2.jpg":th_id, @"Sea2":th_name, @"#69d2e7":th_top, @"#69d2e7":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"skyline.jpg":th_id, @"Skyline":th_name, @"#000000":th_top, @"#000000":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"skyflower.jpg":th_id, @"Skyflower":th_name, @"#f0b09b":th_top, @"#f0b09b":th_bottom, @"#ffffff":th_font, @"#ffffff":th_main};
+    return @{@"tower.jpg":th_id, @"Tower":th_name, @"#aaaaaa":th_top, @"#aaaaaa":th_bottom, @"#ffffff":th_font, @"#000000":th_main};
+    
 }
 
 @end
