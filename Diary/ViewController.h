90b733aa04a1ce90dce2c9a14e32eec795e9e2ce
mju <mju@mjuui-macbook-air-2.local>
2016-07-13 18:31:43 +0900

    test

--- Diary/ViewController.h
+++ Diary/ViewController.h
@@ -14,7 +14,6 @@
 - (IBAction)buttonTouch:(id)sender;
 - (IBAction) digitPressed:(UIButton *)sender;
 - (IBAction) operationPressed:(UIButton *)sender;
-
 @property (weak, nonatomic) IBOutlet UILabel *displayLabel;
 @end
  
